b90c4bdf445ae8e3b844fbcd5da64c1b1287c1f8
Suresh Dobariya <sureshd@systemintegration.in>
2023-01-04 17:58:50 +0530

    LoginRegister after sucesslogin Home Screen managed.

--- lib/components/HomeScreen.dart
+++ lib/components/HomeScreen.dart
@@ -4,32 +4,42 @@ class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
+  final textFieldValueHolder = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-       
-          child: Container(
-          height: 80,
-          width: 150,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-          child: TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'LogOut',
-              style: TextStyle(color: Colors.white, fontSize: 25),
-            ),
-          ),
-          
+        appBar: AppBar(
+          title: Text('Home Page'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 80,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child:
+                      Text("admin@gmail.com", style: TextStyle(fontSize: 20)))
+            ],
+          ),
+        ));
   }
 }
